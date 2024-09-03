45ca3ecb0750ba3c6a5b1da123cf0c20e47166f0
Kaja Skraamo <rub017@uib.no>
2024-09-03 10:29:03 +0200

    first commit

---
+++ src/test/java/com/example/Expass/ExpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Expass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
